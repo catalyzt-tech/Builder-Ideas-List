46535e15d78ce3d6b44e4b05291c4fbff5b04f7a
Billy191 <bill.ratchanon@gmail.com>
2024-02-25 18:15:51 +0700

    feat: draft landing page

---
+++ component/Navbar.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div className="font-medium">
+      <Link href="/">This Is Navbar</Link>
+    </div>
+  )
+}
+export default Navbar
